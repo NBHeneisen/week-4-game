057f786cb45edf9448d0d85a8aa1e4e61f35f2cd
Nicholas Heneisen <Nicholas Heneisen>
2017-02-23 13:25:29 -0500

    method tweaking and debugging

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,13 @@
-var crystalCollector = {
-	randGuessRange: [1,12],	//creates a range for the buttons, easily editable to make the range different.
+var collector = {
+	//randGuessRange: [1,12],	//creates a range for the buttons, easily editable to make the range different.
 
 	//the buttons and goal, using the randomNumButton function to generate random numbers
-	//button1: this.randomNumButton(this.randGuessRange[0],this.randGuessRange[1]),
-	//button2: this.randomNumButton(this.randGuessRange[0],this.randGuessRange[1]),
-	//button3: this.randomNumButton(this.randGuessRange[0],this.randGuessRange[1]),
-	//button4: this.randomNumButton(this.randGuessRange[0],this.randGuessRange[1]),
-	goal: this.randomNumButton(19,120),
+
+	button1: randomNumButton(1,12),
+	button2: randomNumButton(1,12),
+	button3: randomNumButton(1,12),
+	button4: randomNumButton(1,12),
+	goal: randomNumButton(19,120),
 
 	//your current number, will be added to by the buttons
 	currentNum: 0,
@@ -15,19 +16,31 @@ var crystalCollector = {
 	wins: 0,
 	losses: 0,
 
-	//runs when the round is over
-	//reset : function () {
-	//	resets the necessary variables
-	//	if currentNum === goal;
-	//		increase wins;
-	//		broadcast You won!!
-	//	else;
-	//		increase losses;
-	//		broadcast You lost!!
-	//},
+
+	//runs when the round is over, resets necessary numbers and increases win/loss counters.
+	reset: function () {		
+		if (this.currentNum === this.goal) {
+			this.wins ++;
+			$("#wins").append("<p>You win!!</p>");
+		} else if (this.currentNum > this.goal) {
+			this.losses ++;
+			$("#losses").append("<p>You Lose!!</p>");
+		}
+
+		this.button1 = randomNumButton(1,12);
+		this.button2 = randomNumButton(1,12);
+		this.button3 = randomNumButton(1,12);
+		this.button4 = randomNumButton(1,12);
+		this.goal = randomNumButton(19,120);
+		this.currentNum = 0;
+	},
 
 	//runs during each button click, adds to your current number, then checks for win/loss conditions
-	//turn: function () {
+	turn: function () {
+		console.log("inside turn function");
+		this.currentNum= "meh";
+		
+
 	//	on button click
 	//		if button 1 pressed;
 	//			increase currentNum by button1
@@ -35,16 +48,21 @@ var crystalCollector = {
 	//			increase by etc.
 	//	if currentNum >= goal;
 	//		reset();
-	//},
-
-	//random number generator, takes in a min and max number and returns a number that meets the conditions
-	randomNumButton: function (min,max) {
-		return Math.floor(Math.random()*(min-max+1)+min);
 	},
 
-}
-console.log(cryatalCollector.button1)
-console.log(cryatalCollector.button2)
-console.log(cryatalCollector.button3)
-console.log(cryatalCollector.button4)
-console.log(cryatalCollector.goal)
+
+};
+
+//random number generator, takes in a min and max number and returns a number that meets the conditions
+function randomNumButton(min,max) {
+	return Math.floor(Math.random()*(max-min+1)+min);
+};
+
+$(".button").on("click",collector.turn());
+//collector.turn(); //initialize game
+
+console.log(collector.button1);
+console.log(collector.button2);
+console.log(collector.button3);
+console.log(collector.button4);
+console.log(collector.goal);
