a893645d3931c72b0513ebe0840867742e8bb988
Nicholas Heneisen <Nicholas Heneisen>
2017-02-24 23:38:01 -0500

    fully functional with basic css styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,8 @@
+window.onload = function() {
+
+$(".goal").html(collector.goal);
+
+};
 var collector = {
 	//randGuessRange: [1,12],	//creates a range for the buttons, easily editable to make the range different.
 
@@ -21,35 +26,23 @@ var collector = {
 	reset: function () {		
 		if (this.currentNum === this.goal) {
 			this.wins ++;
-			$("#wins").append("<p>You win!!</p>");
+			$("#wins").html(collector.wins);
+			$(".winning").html("<p>You win!!!</p>");
 		} else if (this.currentNum > this.goal) {
 			this.losses ++;
-			$("#losses").append("<p>You Lose!!</p>");
-		}
+			$("#losses").html(collector.losses);
+			$(".losing").html("<p>You lose!!!</p>");
+		};
 
 		this.button1 = randomNumButton(1,12);
 		this.button2 = randomNumButton(1,12);
 		this.button3 = randomNumButton(1,12);
 		this.button4 = randomNumButton(1,12);
 		this.goal = randomNumButton(19,120);
+		$(".goal").html(collector.goal);
+
 		this.currentNum = 0;
 	},
-
-	//runs during each button click, adds to your current number, then checks for win/loss conditions
-	turn: function () {
-		console.log("inside turn function");
-		this.currentNum= "meh";
-		
-
-	//	on button click
-	//		if button 1 pressed;
-	//			increase currentNum by button1
-	//		else if button 2 pressed;
-	//			increase by etc.
-	//	if currentNum >= goal;
-	//		reset();
-	},
-
 
 };
 
@@ -58,11 +51,22 @@ function randomNumButton(min,max) {
 	return Math.floor(Math.random()*(max-min+1)+min);
 };
 
-$(".button").on("click",collector.turn());
-//collector.turn(); //initialize game
+function reply_click(clicked_id) {
+	$(".winning").html("");
+	$(".losing").html("");
+	console.log(clicked_id);
+	collector.currentNum += collector[clicked_id];
+	$(".score").html(collector.currentNum);
+	console.log(collector.currentNum);
+	if (collector.currentNum >= collector.goal) {
+		collector.reset();
+	};
+};
+
 
 console.log(collector.button1);
 console.log(collector.button2);
 console.log(collector.button3);
 console.log(collector.button4);
 console.log(collector.goal);
+
